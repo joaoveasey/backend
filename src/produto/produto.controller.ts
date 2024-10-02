9007ec85f0da9de43146123bd86638d54f41954e
joaoveasey <jovepedro@gmail.com>
2024-10-01 22:58:15 -0300

    feat: iniciando a intergração do backend com o frontend

---
+++ src/produto/produto.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { Produto, produtos } from 'src/core';
+
+@Controller('produtos')
+export class ProdutoController {
+    @Get()
+    obterProdutos(): Produto[] {
+        return produtos.map((produto) => ({
+            ...produto,
+            especificacoes: { destaque: produto.especificacoes.destaque }
+        }));
+    }
+}
